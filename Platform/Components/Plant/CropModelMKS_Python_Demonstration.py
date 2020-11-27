525dc1a255f43c3bb101756aad074d95ad43ad3d
SongJim624 <44838018+SongJim624@users.noreply.github.com>
2020-11-27 09:08:31 +0800

    The components of the CropModelMKS

---
+++ Platform/Components/Plant/CropModelMKS_Python_Demonstration.py
@@ -0,0 +1,26 @@
+import win32com.server
+import win32com.client
+
+class State:
+    def __init__(self):
+        self.single = 0
+        self.array = []
+        return
+
+class Parameter:
+    def __init__(self):
+        self.single = 0
+        self.array = []
+        return
+
+class Dependency:        
+    def __init__(self):
+        return
+
+class Component:
+    def __init__():
+        return
+
+
+
+
